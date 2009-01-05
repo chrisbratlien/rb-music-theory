99b9e91f5b37f1eb1a30cb303abc2f0873cb6930
unknown <User@.(none)>
2009-01-05 07:26:05 -0600

    no lower base_duration than 0.75

--- examples/olympiano/launcher.rb
+++ examples/olympiano/launcher.rb
@@ -44,7 +44,8 @@ def perform(attributes = {})
     if @logging
       puts "queueing #{chord_text} on degree #{degree}  improv: #{improv_key}"
     end
-    base_duration *= ( 1 + (rand(30)-15)* 0.01)  # if you're at 1.0, jump to somewhere between 0.85 and 1.15
+    base_duration *= ( 1 + (rand(30)-15)* 0.01)  
+    base_duration = [base_duration,0.75].max # go no lower than 0.75
 
     mini_queue = []
 	  @improv_set[improv_key][chord.note_values,base_duration,mini_queue]  #picking an improv lambda and then invoking it 
